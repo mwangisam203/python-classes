abf6602b60a73ee98f27f10bc78b8ebe2af36ffc
mwangisam203 <mwangisam203@gmail.com>
2026-02-03 03:04:56 -0500

    broken c

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ class Shape:
         self.filled = filled
         
 
-class Circle:
+class Circle(Shape):
     def __init__(self, color, filled, radius):
         super().__init__(color, filled)
         self.radius = radius
 
         
 
-class Square:
+class Square(Shape):
     def __init__(self, color, filled, width):
         super().__init__(color, filled)
-        self.radius = width
+        self.width = width
     
-class Triangle:
+class Triangle(Shape):
     def __init__(self, color, filled, width, height):
         super().__init__(color, filled)
-        self.radius = width
+        self.width = width
         self.height = height
+
+
+circle = Circle(color="grey", filled = True , radius=7)
+
+print(circle.color)
