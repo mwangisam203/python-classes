6e6792c558298d1aafc9a989c6d34402ead826b8
mwangisam203 <mwangisam203@gmail.com>
2026-02-03 02:45:11 -0500

    adj

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ class Circle:
         
 
 class Square:
-    def __init__(self, color, filled, radius):
+    def __init__(self, color, filled, width):
         self.color = color
         self.filled = filled
-        self.radius = radius
+        self.radius = width
     
 class Triangle:
-    def __init__(self, color, filled, radius):
+    def __init__(self, color, filled, width, height):
         self.color = color
         self.filled = filled
-        self.radius = radius
+        self.radius = width
+        self.height = height
