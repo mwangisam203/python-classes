d5dcb8f0e2e4e61418c97afdf0a8c201fa65783c
mwangisam203 <mwangisam203@gmail.com>
2026-02-03 02:50:25 -0500

    construct super class

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #super(): =>function used in a child class to call methods from the parent class (superclass)
 
-class Circle:
-    def __init__(self, color, filled, radius):
+class Shape:
+    def __init__(self, color, filled):
         self.color = color
         self.filled = filled
+        
+
+class Circle:
+    def __init__(self, color, filled, radius):
+        super().__init__(color, filled)
         self.radius = radius
 
         
 
 class Square:
     def __init__(self, color, filled, width):
-        self.color = color
-        self.filled = filled
+        super().__init__(color, filled)
         self.radius = width
     
 class Triangle:
     def __init__(self, color, filled, width, height):
-        self.color = color
-        self.filled = filled
+        super().__init__(color, filled)
         self.radius = width
         self.height = height
